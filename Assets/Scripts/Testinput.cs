6aa14266c5245b280921c92b5412e1aa4d42dc1f
Kevin J <kjakklc@yahoo.com>
2019-02-19 22:10:41 -0500

    Update-Prefabs,test control(movement) scripts, and asset updates

---
+++ Assets/Scripts/Testinput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+ 
+public class InputTest: MonoBehaviour
+{
+    public SteamVR_Action_Boolean TriggerClick;
+    private SteamVR_Input_Sources inputSource;
+ 
+    private void Start() {} //Monobehaviours without a Start function cannot be disabled in Editor, just FYI
+ 
+    private void OnEnable()
+    {
+        TriggerClick.AddOnStateDownListener(Press, inputSource);
+    }
+ 
+    private void OnDisable()
+    {
+        TriggerClick.RemoveOnStateDownListener(Press, inputSource);
+    }
+ 
+    private void Press(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //put your stuff here
+        print("Success");
+    }
+}
