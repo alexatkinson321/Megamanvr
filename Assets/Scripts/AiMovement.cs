985487aa580b388dce644300ee96e83ca1147a84
Shot1200 <mahummel@oakland.edu>
2019-02-26 20:35:51 -0500

    Added the following: - Models for bullets - sound fx, and background music - scripts to handle shooting bullets using raycasting, will remove once finished with bullet transformations - scripts to handle sound fx - scripts to handle health and respawn, (WIP) - skybox and plain grass for environment placeholders - target box for destroying with respawn script - moving box for AI movement testing

---
+++ Assets/Scripts/AiMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AiMovement : MonoBehaviour
+{
+    public float Speed = 3f;
+    public float rotSpeed = 100f;
+    private bool movingAI = false;
+    private bool movingRight = false;
+    private bool movingLeft = false;
+    private bool movingAround = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+   void Update()
+    {
+
+        if (movingAI == false)
+        {
+            StartCoroutine(AIMovement());
+        }
+
+        if(movingRight == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * rotSpeed);
+        }
+
+        if (movingLeft == true)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -rotSpeed);
+        }
+
+        if (movingAround == true)
+        {
+            transform.position += transform.forward * Speed * Time.deltaTime;
+        }
+    }
+
+    IEnumerator AIMovement()
+    {
+        int rotTime = Random.Range(1, 3);
+        int rotWait = Random.Range(1, 3);
+        int rotLorR = Random.Range(0, 3);
+        int walkWait = Random.Range(1, 3);
+        int walkTime = Random.Range(1, 5);
+
+        movingAI = true;
+
+        yield return new WaitForSeconds(walkWait);
+
+        movingAround = true;
+
+        yield return new WaitForSeconds(walkTime);
+
+        movingAround = false;
+
+        yield return new WaitForSeconds(rotWait);
+
+        if (rotLorR == 1)
+        {
+            movingRight = true;
+            yield return new WaitForSeconds(rotTime);
+            movingLeft = true;
+        }
+
+        if (rotLorR == 2)
+        {
+            movingLeft = true;
+            yield return new WaitForSeconds(rotTime);
+            movingRight = true;
+        }
+
+        movingAI = false;
+    }
+}
---
+++ Assets/Scripts/MegaBuster.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MegaBuster : MonoBehaviour
+{
+    private SteamVR_TrackedController controller;
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform busterEnd;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    public AudioClip busterSound;
+    public AudioClip busterCharge;
+    private AudioSource source;
+    
+    private LineRenderer busterLine;
+    private float nextFire;
+    public GameObject origin;
+    
+    void Start()
+    {
+        busterLine = GetComponent<LineRenderer>();
+        //busterSound = GetComponent<AudioSource>();
+        source = GetComponent<AudioSource>();    
+    }
+    
+    private void OnEnable(){
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += HandleTriggerClicked;
+
+    }
+
+    private void OnDisable(){
+
+        controller.TriggerClicked -= HandleTriggerClicked;
+
+
+    }
+
+
+    #region fire
+    private void HandleTriggerClicked(object sender, ClickedEventArgs e){
+        Fire();
+
+    }
+
+    private void Fire(){
+        if (Time.time > nextFire){
+            nextFire = Time.time + fireRate;
+            StartCoroutine(shotEffect());
+            Vector3 rayOrigin = origin.transform.position;
+            RaycastHit hit;
+            busterLine.SetPosition(0, busterEnd.position);
+            if (Physics.Raycast(rayOrigin, origin.transform.forward, out hit, weaponRange)){
+                busterLine.SetPosition(1 , hit.point);
+            }
+        else {
+                busterLine.SetPosition(1 , rayOrigin + (origin.transform.forward * weaponRange));
+            }
+        }
+    }
+    #endregion
+
+    // void Update()
+    // {
+    //     if (controller.triggerPressed && Time.time > nextFire){
+    //         nextFire = Time.time + fireRate;
+    //         StartCoroutine(shotEffect());
+    //         Vector3 rayOrigin = origin.transform.position;
+    //         RaycastHit hit;
+    //         busterLine.SetPosition(0, busterEnd.position);
+    //         if (Physics.Raycast(rayOrigin, origin.transform.forward, out hit, weaponRange)){
+
+    //                 busterLine.SetPosition(1 , hit.point);
+    //         }
+    //         else {
+    //                 busterLine.SetPosition(1 , rayOrigin + (origin.transform.forward * weaponRange));
+
+    //         }
+    //     }
+    // } 
+
+    private IEnumerator shotEffect(){
+        source.PlayOneShot(busterSound, 1f);
+        busterLine.enabled = true;
+        yield return shotDuration;
+        busterLine.enabled = false;
+    }
+}
