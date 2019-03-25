fbcdd445e304d217885f46a3656192f431f3804c
Kevin J <kjakklc@yahoo.com>
2019-03-25 13:21:10 -0400

    Backup from Midterm1

---
+++ Assets/Scripts/LocoMotion.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+//Locomotion movement: Move controllers up and down to transverse level
+//How to use:
+//Add locomotion script component to camera rig
+//(Might need to) Create empty gameobject on scene and attach component Locomotion
+//Drag CameraRig to Camera Rig in Loco Script
+//Drag Camera(eye) to headset
+//Drag controller(left) and (right) to Left and Right Loco Script
+
+public class LocoMotion : MonoBehaviour
+{
+
+    //Control over full headset
+    public GameObject headset; //not tracked obj
+    public GameObject CameraRig;
+    public SteamVR_TrackedObject Left; //Left controller
+    public SteamVR_TrackedObject Right; //Right controller
+
+    private Vector3 CameraRigloc;  //location of camerarig
+    private Vector3 prevLeft; //previous position of left controller
+    private Vector3 prevRight; //previous position of right controller
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetPrevState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Velocity where its moving
+        Vector3 rightVal = Right.transform.position - prevRight;
+        Vector3 leftVal = Right.transform.position - prevRight;
+
+        float totalVal =+ rightVal.magnitude*0.8f + leftVal.magnitude*0.8f;
+        //Getting input when key pressed in SteamVR (Inputs from both controllers)
+        var device = SteamVR_Controller.Input((int)Left.index);
+        var device2 = SteamVR_Controller.Input((int)Right.index);
+        //CameraRig.transform.GetComponent;
+        //if total value is >= 
+        if(totalVal >= 0.05)
+        {
+            //Uses Grip button - 8 on controller diagram
+            //When either or grip buttons used
+            if(device.GetTouch(SteamVR_Controller.ButtonMask.Grip) || device2.GetTouch(SteamVR_Controller.ButtonMask.Grip))
+             //if(device.GetTouch(SteamVR_Controller.ButtonMask.Grip) && device2.GetTouch(SteamVR_Controller.ButtonMask.Grip))
+            //Move in direction of headset facing
+            //Alter value for speed transition
+            CameraRig.transform.position += headset.transform.forward / 8;
+        }
+
+        //at end of every update setprev state resetting old state
+        SetPrevState(); 
+        //Debug
+        //Debug.Log(totalVal);
+    }
+
+    //Prev positions of controllers
+    void SetPrevState()
+    {
+        //Get Current positions - call at end of frame
+        prevLeft = Left.transform.position;
+        prevRight = Right.transform.position;
+    }
+}
---
+++ Assets/Scripts/myTouchpad.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+ //Uses Touchpad to move
+ //Add myTouchpad script to controller either left or right (left preferred)
+ //Drag CameraRig (from samplescene) to player in the mytouchpad component script - to attach camerarig to player
+ //(Might need) Rigidbody component for rotation and position
+
+ public class myTouchpad : MonoBehaviour
+ {
+     //Player -CameraRig
+     public GameObject player; //not tracked obj
+     public SteamVR_Controller.Device device;
+     public SteamVR_TrackedObject controller; //Controller
+     public Vector2 touchpad;
+ 
+     private float sensitivityX = 2.0F;
+     //private float sensitivity = 3.5f
+     private Vector3 playerPosition;
+ 
+     void Start()
+     {
+         /*trackedobj = GetComponent<SteamVR_TrackedObject>();
+         device = SteamVR_Controller.Input((int)trackedobj.index);*/
+         controller = gameObject.GetComponent<SteamVR_TrackedObject>();
+     }
+ 
+     // Update is called once per frame
+     void Update()
+     {
+         device = SteamVR_Controller.Input((int)controller.index);
+         //If touchpad is pressed
+         //Button 2 on Vive Controller
+         if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+         {
+             //Reads the Vive touchpad values
+             touchpad = device.GetAxis(EVRButtonId.k_EButton_SteamVR_Touchpad);
+ 
+ 
+             //Handle movement via touchpad
+             if (touchpad.y > 0.2f || touchpad.y < -0.2f) {
+                 //Move Forward
+                 player.transform.position -= player.transform.forward * Time.deltaTime * (touchpad.y * 5f);
+ 
+                 //Adjust the height to terrain height based where player positin
+                 playerPosition = player.transform.position;
+                 playerPosition.y = Terrain.activeTerrain.SampleHeight(player.transform.position);
+                 player.transform.position = playerPosition;
+             }
+ 
+             //Handles the rotation by touchpad
+             if (touchpad.x > 0.25f || touchpad.x < -0.25f) {
+                 player.transform.Rotate(0, touchpad.x * sensitivityX, 0);
+             }
+
+                //Debug testing x and y location log
+             //Debug.Log ("Touchpad X = " + touchpad.x + " : Touchpad Y = " + touchpad.y);
+         }
+     }
+ }
